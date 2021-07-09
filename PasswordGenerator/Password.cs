c2ae4f27600357f9c5f213104f8997cfd5c4455c
Callum Lockley <lockleycallum1@gmail.com>
2021-07-09 10:42:19 +0100

    Finished method for generating random password without special characters

--- PasswordGenerator/Password.cs
+++ PasswordGenerator/Password.cs
@@ -6,16 +6,28 @@ using System.Threading.Tasks;
 
 namespace PasswordGenerator
 {
-    class Password
+    public class Password
     {
-        private string generatedPass;
-        public string GeneratePassword(int passSize)
+        public static string GeneratePassword(int passSize)
         {
             if (passSize < 0)
+            {
+                Console.WriteLine("Your password length must be greater than zero");
                 throw new ArgumentException("length must be greater than zero");
-            
-            
-            return generatedPass;
+
+            }
+            const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            StringBuilder sb = new StringBuilder();
+            Random rnd = new Random();
+
+            for (int i = 0; i < passSize; i++)
+            {
+                int index = rnd.Next(chars.Length);
+                sb.Append(chars[index]);
+            }
+
+            return sb.ToString();
 
         }
     }
--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -2,19 +2,23 @@
 
 namespace PasswordGenerator
 {
-    class Program
+    public class Program
     {
         public static int passSize;
-        public string genPass;
+        public static string genPass;
         
         static void Main(string[] args)
         {
             Console.WriteLine("How many characters does the password need?");
             passSize = Convert.ToInt32(Console.ReadLine());
+
+            genPass  = Password.GeneratePassword(passSize);
+
+            Console.WriteLine(genPass.ToString());
         }
 
 
 
-        
+
     }
 }
